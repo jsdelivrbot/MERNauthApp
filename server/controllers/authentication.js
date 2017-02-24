fd8e10a0c6f7186aa6cbd8a6dc2308b37727100e
Ander Conal <ander.conal@jinnapp.com>
2017-02-24 18:18:54 +0100

    Remove unused console.log

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -36,7 +36,6 @@ exports.signup = function(req, res, next) {
         return next(err);
         // Respond to request indicating the user was created
         res.json({ token: tokenForUser(user) });
-        console.log(tokenForUser(user));
       }
     });
   });
